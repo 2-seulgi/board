99fe0893d22ae437f3cfb612c9596b79aad6abec
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-08-24 22:31:46 +0900

    ✨ 게시판 CRUD API (READ)
    게시물을 보기 위한 간단한 REST API - READ

--- src/main/java/com/toy/board/controller/PostController.java
+++ src/main/java/com/toy/board/controller/PostController.java
@@ -1,2 +1,41 @@
-package com.toy.board.controller;public class PostController {
+package com.toy.board.controller;
+
+import com.toy.board.model.Post;
+import com.toy.board.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PostController {
+
+    @Autowired private PostService postService;
+
+    @GetMapping("/api/v1/posts")
+    public ResponseEntity<List<Post>> getPosts(){
+        List<Post> posts = postService.getPosts();
+
+        return ResponseEntity.ok(posts);
+    }
+
+
+    @GetMapping("/api/v1/posts/{postId}")
+    public ResponseEntity<Post> getPost(
+            @PathVariable Long postId
+    ){
+        Optional<Post> matchingPost = postService.getPost(postId);
+
+        return matchingPost
+                .map(ResponseEntity::ok)
+                .orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/com/toy/board/service/PostService.java
+++ src/main/java/com/toy/board/service/PostService.java
@@ -1,2 +1,31 @@
-package com.toy.board.service;public class PostService {
+package com.toy.board.service;
+
+import com.toy.board.model.Post;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private static final List<Post> posts = new ArrayList<>();
+
+    static {
+        posts.add(new Post(1L, "post 1", ZonedDateTime.now()));
+        posts.add(new Post(2L, "post 2", ZonedDateTime.now()));
+        posts.add(new Post(3L, "post 3", ZonedDateTime.now()));
+
+    }
+
+    public List<Post> getPosts(){
+        return posts;
+    }
+
+    public Optional<Post> getPost(Long postId){
+        return posts.stream().filter(post -> postId.equals(post.getPostId()))
+                .findFirst();
+    }
 }
+
