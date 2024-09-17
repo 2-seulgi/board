c84228b04342d51fb7ad1f0f3380b2f5e0bbdfaa
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-09-17 23:43:27 +0900

    ✨ Jwt 인증 관련 설정 및 필터 추가

---
+++ src/main/java/com/toy/board/config/JwtAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.toy.board.config;
+
+import com.toy.board.exception.jwt.JwtTokenNotFoundException;
+import com.toy.board.service.JwtService;
+import com.toy.board.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired private UserService userService;
+    @Autowired private JwtService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String BEARER_PREFIX = "Bearer ";
+        var authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        var securityContext = SecurityContextHolder.getContext();
+        if(ObjectUtils.isEmpty(authorization)|| !authorization.startsWith(BEARER_PREFIX)){
+            throw new JwtTokenNotFoundException();
+        }
+        if (!ObjectUtils.isEmpty(authorization) && authorization.startsWith(BEARER_PREFIX)
+        && securityContext.getAuthentication() == null){
+            var accessToken = authorization.substring(BEARER_PREFIX.length());
+             var username = jwtService.getUsername(accessToken);
+            var userDetails = userService.loadUserByUsername(username);
+
+            var authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities()
+            );
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            securityContext.setAuthentication(authenticationToken);
+            SecurityContextHolder.setContext(securityContext);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+}
---
+++ src/main/java/com/toy/board/config/JwtExceptionFilter.java
@@ -0,0 +1,46 @@
+package com.toy.board.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try{
+            filterChain.doFilter(request, response);
+        }catch (JwtException exception){
+            //  jwt 관련 커스텀 에러 메세지 생성
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setCharacterEncoding("UTF-8");
+
+            // JSON으로 응답할 오류 메시지
+            var errorMap = new HashMap<String, Object>();
+            errorMap.put("status", HttpStatus.UNAUTHORIZED);
+            errorMap.put("message", exception.getMessage());
+
+            // ObjectMapper를 사용해 Map을 JSON 형식으로 변환
+            ObjectMapper objectMapper = new ObjectMapper();
+            String responseJson = objectMapper.writeValueAsString(errorMap);
+            response.getWriter().write(responseJson);
+
+        }
+
+    }
+}
